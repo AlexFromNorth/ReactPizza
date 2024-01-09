d13a6060202f8d288e0081ba12cd904ee489b794
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2024-01-09 18:17:14 +0300

    added useEffect to fullPizza with axios request

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const FullPizza = () => {
-    const {id} = useParams()
+  const { id } = useParams();
+
+  useEffect(()=>{
+    axios.get('https://6525522667cfb1e59ce71807.mockapi.io/items/' + id)
+      .then(res){
+        console.log(res)
+      }
+  },[])
 
   return (
-    <div className='container'>
-        <img src="" alt="picture" />
-        <h2>{id}</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur veritatis nisi maxime dignissimos debitis eius ipsa ex voluptatum porro recusandae sunt fugiat, quo optio laborum obcaecati fuga mollitia, blanditiis velit?</p>
-        <h4>250 руб</h4>
+    <div className="container">
+      <img src="" alt="picture" />
+      <h2>{id}</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+        veritatis nisi maxime dignissimos debitis eius ipsa ex voluptatum porro
+        recusandae sunt fugiat, quo optio laborum obcaecati fuga mollitia,
+        blanditiis velit?
+      </p>
+      <h4>250 руб</h4>
     </div>
-  )
-}
+  );
+};
 
-export default FullPizza
+export default FullPizza;
