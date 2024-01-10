a2e3e8cc7c09dd49ffd4177f2ec6c4435721ac28
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2024-01-10 22:03:15 +0300

    added ts for pizza item

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -3,7 +3,11 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const FullPizza = () => {
-  const [pizza, setPizza] = useState();
+  const [pizza, setPizza] = useState<{
+    imageUrl: string;
+    title: string;
+    price: number;
+  }>();
   const { id } = useParams();
 
   useEffect(() => {
@@ -24,7 +28,7 @@ const FullPizza = () => {
 
   return (
     <div className="container center">
-      <img src={pizza.imageUrl} alt="picture" />
+      <img src={pizza.imageUrl} alt="picture" className="w30"/>
       <h2 className="mt-10">{pizza.title}</h2>
       <h4 className="mt-10">{pizza.price} руб</h4>
     </div>
