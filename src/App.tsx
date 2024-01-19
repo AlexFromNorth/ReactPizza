b705b2e6b5493f4ed94eb903c3b18e19f37a5ef9
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2024-01-19 16:54:07 +0300

    created lazy load for Cart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Header from "./components/header/Header";
 import "./scss/app.scss";
-import React, { createContext, useEffect, useState } from "react";
+import React, { Suspense, createContext, useEffect, useState } from "react";
 import Skeleton from "./components/pizzaBlock/Skeleton";
 import Home from "./pages/Home";
 import NotFoundBlock from "./components/notFoundBlock/NotFoundBlock";
 import NotFound from "./pages/NotFound";
 import { Route, Routes } from "react-router-dom";
-import Cart from "./pages/cart/cart";
+// import Cart from "./pages/cart/cart";
 import FullPizza from "./pages/FullPizza";
+
+import { lazy } from "react";
+
+const Cart = lazy(()=>import("./pages/cart/Cart"))
 
 export const SearchContext = createContext();
 
@@ -16,6 +20,7 @@ function App() {
 
   return (
     <div className="wrapper">
+      
       <SearchContext.Provider value={{ handlerLogo, setHandlerLogo }}>
         <Header />
         <div className="content">
@@ -23,7 +28,7 @@ function App() {
           <Routes>
             <Route path="*" element={<NotFound />} />
             <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/cart" element={<Suspense fallback={<div>Загрузка...</div>}><Cart /></Suspense>} />
             <Route path="/pizza/:id" element={<FullPizza />} />
           </Routes>
           {/* </div> */}
